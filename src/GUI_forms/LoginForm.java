58cea014c850119bc3110054325531a49b175bf3
jaguel <m.jagiello03@gmail.com>
2024-08-25 13:06:03 +0200

    Implementation of 'LoginForm' form

---
+++ src/GUI_forms/LoginForm.java
@@ -0,0 +1,89 @@
+package GUI_forms;
+
+import javax.swing.*;
+
+public class LoginForm extends JFrame {
+    private JTextField loginField;
+    private JPasswordField passwordField;
+    private JButton signInButton;
+    private JButton signUpButton;
+    private JLabel message;
+    private JPanel contentPane;
+
+    private boolean signIn = false;
+    private boolean signUp = false;
+
+    public LoginForm() {
+        setTitle("Panel logowania");
+        setContentPane(contentPane);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+
+        setVisible(true);
+
+        signInButton.addActionListener(_ -> onSignIn());
+        signUpButton.addActionListener(_ -> onSignUp());
+    }
+
+    private void onSignUp() {
+        if (loginField.getText().isBlank() || !passwordField.echoCharIsSet())
+            setMessage("Pola nie mogą być puste");
+        else {
+            JPasswordField confirmPasswordField = new JPasswordField(10);
+            int option = JOptionPane.showConfirmDialog(this, confirmPasswordField,
+                    "Potwierdź hasło", JOptionPane.OK_CANCEL_OPTION);
+
+            if (option == JOptionPane.OK_OPTION) {
+                String password = new String(passwordField.getPassword());
+                String confirmPassword = new String(confirmPasswordField.getPassword());
+
+                if (password.equals(confirmPassword)) {
+                    signUp = true;
+                    setMessage("Pomyślnie zarejestrowano");
+                } else {
+                    setMessage("Hasła się nie zgadzają");
+                }
+            }
+        }
+    }
+
+    private void onSignIn() {
+        if (loginField.getText().isBlank() || !passwordField.echoCharIsSet())
+            setMessage("Pola nie mogą być puste");
+        else
+            signIn = true;
+    }
+
+    public void setMessage(String message) {
+        this.message.setText(message);
+    }
+
+    public String getLogin() {
+        return loginField.getText();
+    }
+
+    public String getPassword() {
+        return new String(passwordField.getPassword());
+    }
+
+    public void setSignIn(boolean isSignIn) {
+        this.signIn = isSignIn;
+    }
+
+    public void setSignUp(boolean isSignUp) {
+        this.signUp = isSignUp;
+    }
+
+    public boolean isSignIn() {
+        return signIn;
+    }
+
+    public boolean isSignUp() {
+        return signUp;
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(LoginForm::new);
+    }
+}
