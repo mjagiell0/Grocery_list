a876900c9cd0746c391b44d87a55c78453eee343
jaguel <m.jagiello03@gmail.com>
2024-08-27 20:37:31 +0200

    Prepared connection between 'Server' and 'Client' classes

--- src/main_classes/Client.java
+++ src/main_classes/Client.java
@@ -1,4 +1,27 @@
 package main_classes;
 
+import GUI_forms.FormsHandler;
+import grocery_classes.GroceryClient;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class Client {
+    public static void main(String[] args) throws IOException {
+        try (Socket socket = new Socket("127.0.0.1", 8080)) {
+            FormsHandler formsHandler = new FormsHandler();
+            GroceryClient groceryClient = new GroceryClient();
+
+            ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
+
+            int statusCode = 1;
+
+            if (statusCode == 1) {
+
+            }
+        }
+    }
 }
--- src/main_classes/Server.java
+++ src/main_classes/Server.java
@@ -1,4 +1,69 @@
 package main_classes;
 
+import notification_classes.Notification;
+import notification_classes.NotificationCode;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.DatagramSocket;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.SQLException;
+
 public class Server {
+    private final static DatabaseHandler databaseHandler;
+
+    static {
+        try {
+            databaseHandler = new DatabaseHandler();
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(2222)) {
+            System.out.println("Server listening on port 2222...");
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("New connection with: " + clientSocket);
+
+                ClientHandler clientHandler = new ClientHandler(clientSocket);
+                new Thread(clientHandler).start();
+
+
+            }
+        } catch (IOException e) {
+            System.out.println("Server exception: " + e);
+        }
+    }
+
+    static class ClientHandler implements Runnable {
+        private final Socket clientSocket;
+
+        ClientHandler(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                ObjectOutputStream outputStream = new ObjectOutputStream(clientSocket.getOutputStream());
+                ObjectInputStream inputStream = new ObjectInputStream(clientSocket.getInputStream());
+
+                while (true) {
+                    Thread.sleep(100);
+                    Notification notification = (Notification) inputStream.readObject();
+                    NotificationCode code = notification.getCode();
+
+
+
+                }
+            } catch (IOException | ClassNotFoundException | InterruptedException e) {
+                System.out.println("Client exception: " + e);
+            }
+        }
+    }
 }
